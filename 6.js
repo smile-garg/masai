11eee687a6a1ed75654988967a15063b75ce6ead
Smile <smile2165.be21@chitkara.edu.in>
2025-02-23 18:50:33 +0530

    6.js

---
+++ 6.js
@@ -0,0 +1,39 @@
+function task1(callback) {
+    setTimeout(() => {
+      console.log("Task 1 complete");
+      callback();
+    }, 1000);
+  }
+  
+  function task2(callback) {
+    setTimeout(() => {
+      console.log("Task 2 complete");
+      callback();
+    }, 1000);
+  }
+  
+  function task3(callback) {
+    setTimeout(() => {
+      console.log("Task 3 complete");
+      callback();
+    }, 1000);
+  }
+  
+  function task4(callback) {
+    setTimeout(() => {
+      console.log("Task 4 complete");
+      callback();
+    }, 1000);
+  }
+  
+  // Callback Hell (Nested Callbacks)
+  task1(() => {
+    task2(() => {
+      task3(() => {
+        task4(() => {
+          console.log("All tasks completed");
+        });
+      });
+    });
+  });
+  
